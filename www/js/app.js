8ad867b433c4e6b35aa0e7a9c0dc97eda8ab3dd3
Kazi Ridwan <ridwan.arefin@gmail.com>
2016-01-10 02:00:29 +0600

    init

---
+++ www/js/app.js
@@ -0,0 +1,65 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.controller('GeoCtrl', function($cordovaGeolocation,$scope) {
+
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+    .then(function (position) {
+      $scope.lat  = position.coords.latitude
+      $scope.long = position.coords.longitude
+    }, function(err) {
+      // error
+    });
+
+
+  var watchOptions = {
+    timeout : 3000,
+    enableHighAccuracy: false // may cause errors if true
+  };
+
+  var watch = $cordovaGeolocation.watchPosition(watchOptions);
+  watch.then(
+    null,
+    function(err) {
+      // error
+    },
+    function(position) {
+      $scope.lat  = position.coords.latitude
+      $scope.long = position.coords.longitude
+  });
+
+
+  watch.clearWatch();
+  // OR
+  // $cordovaGeolocation.clearWatch(watch)
+  //   .then(function(result) {
+  //     // success
+  //     $scope.lat = result.lat
+  //     $scope.lng = result.lng
+  //     }, function (error) {
+  //     // error
+  //   });
+});
